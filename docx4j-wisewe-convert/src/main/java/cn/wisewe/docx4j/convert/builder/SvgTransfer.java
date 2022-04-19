f322029f7e87d6a874184f9ab90fd5314e8aa323
PasseRR <xie__hai@sina.com>
2022-04-19 13:54:15 +0800

    svg转pdf优化

--- docx4j-wisewe-convert/src/main/java/cn/wisewe/docx4j/convert/builder/SvgTransfer.java
+++ docx4j-wisewe-convert/src/main/java/cn/wisewe/docx4j/convert/builder/SvgTransfer.java
@@ -3,6 +3,7 @@ package cn.wisewe.docx4j.convert.builder;
 import cn.wisewe.docx4j.convert.ConvertException;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.AccessLevel;
@@ -19,6 +20,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.BiConsumer;
@@ -83,6 +85,9 @@ public class SvgTransfer {
             PdfWriter writer = PdfWriter.getInstance(document, outputStream);
             document.open();
             float height = document.getPageSize().getHeight(), width = document.getPageSize().getWidth();
+            // 图片缓存
+            List<Image> list = new ArrayList<>();
+            float totalHeight = 0, maxWidth = 0;
             // svg转为pdf的一页
             int index = 0;
             for (byte[] it : this.supplier.get()) {
@@ -90,7 +95,6 @@ public class SvgTransfer {
                 document.add(new Chunk(""));
                 Elements svg = Jsoup.parse(new String(it, StandardCharsets.UTF_8), PARSER).select("svg");
                 this.consumer.accept(index++, svg);
-
                 try (StringReader reader = new StringReader(svg.toString());
                      ByteArrayOutputStream baos = new ByteArrayOutputStream(1024)) {
                     TranscoderInput input = new TranscoderInput(reader);
@@ -98,14 +102,23 @@ public class SvgTransfer {
                     new PNGTranscoder().transcode(input, output);
                     Image image = Image.getInstance(baos.toByteArray());
                     image.scaleToFit(document.getPageSize());
-                    // 设置图片绝对位置
-                    image.setAbsolutePosition(
-                        (width - image.getScaledWidth()) / 2,
-                        (height - image.getScaledHeight()) / 2
-                    );
-                    writer.getDirectContent().addImage(image);
-                    document.newPage();
+                    // 是否可以将多张图片合并在一页pdf
+                    if (totalHeight + image.getScaledHeight() > height) {
+                        this.render(list, writer, height, totalHeight, width - maxWidth);
+                        maxWidth = image.getScaledWidth();
+                        totalHeight = image.getScaledHeight();
+                        list.clear();
+                        document.newPage();
+                    } else {
+                        totalHeight += image.getScaledHeight();
+                        maxWidth = Float.max(maxWidth, image.getScaledWidth());
+                    }
+                    list.add(image);
                 }
+            }
+
+            if (!list.isEmpty()) {
+                this.render(list, writer, height, totalHeight, width - maxWidth);
             }
         } catch (Exception e) {
             throw new ConvertException(e);
@@ -113,6 +126,33 @@ public class SvgTransfer {
             if (document.isOpen()) {
                 document.close();
             }
+        }
+    }
+
+    /**
+     * 按照图片组渲染
+     * @param list        图片组
+     * @param writer      {@link PdfWriter}
+     * @param height      纸张高度
+     * @param totalHeight 图片总高度
+     * @param w           纸张宽度和图片最大宽度差值
+     * @throws DocumentException pdf异常
+     */
+    protected void render(List<Image> list, PdfWriter writer, float height, float totalHeight, float w)
+        throws DocumentException {
+        // 临时高度
+        float th = height;
+        // 若高度大于两倍总高度 则取页面高度的一半
+        while (th > (totalHeight + totalHeight)) {
+            th -= totalHeight;
+        }
+        // 高度补差
+        float h = (th - totalHeight) / (list.size() + 1), t = height - h;
+        for (Image value : list) {
+            value.setAbsolutePosition(w / 2, t - value.getScaledHeight());
+            writer.getDirectContent().addImage(value);
+            t -= value.getScaledHeight();
+            t -= h;
         }
     }
 }
