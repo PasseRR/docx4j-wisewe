c0b0b53e56e86b0e9eda84121e4c47435f8cd97a
PasseRR <xie__hai@sina.com>
2022-06-15 16:58:00 +0800

    使用shade独立依赖

--- docx4j-wisewe-convert/src/main/java/cn/wisewe/docx4j/convert/builder/portable/PortableHandler.java
+++ docx4j-wisewe-convert/src/main/java/cn/wisewe/docx4j/convert/builder/portable/PortableHandler.java
@@ -1,7 +1,7 @@
 package cn.wisewe.docx4j.convert.builder.portable;
 
 import cn.wisewe.docx4j.convert.ConvertHandler;
-import cn.wisewe.docx4j.convert.builder.Asposes;
+import cn.wisewe.docx4j.convert.builder.Licenses;
 import com.aspose.pdf.Document;
 import com.aspose.pdf.License;
 
@@ -23,7 +23,7 @@ abstract class PortableHandler implements ConvertHandler {
 
     @Override
     public void handle(BufferedInputStream inputStream, OutputStream outputStream) throws Exception {
-        Asposes.tryLoadLicense(HAS_LICENSED, new License()::setLicense);
+        Licenses.tryLoadLicense(HAS_LICENSED, new License()::setLicense);
         this.postHandle(new Document(inputStream), outputStream);
     }
 
